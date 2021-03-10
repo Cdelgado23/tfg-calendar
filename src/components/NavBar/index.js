701fcc82114cf2f52b77294eed36c34539805a6d
Carlos Delgado <cdelgadoguiberteau@gmail.com>
2021-03-11 00:41:53 +0100

    feat: initial approach to schedule screen layout

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -13,9 +13,12 @@ const Navbar = () => {
   return (
     <>
       <Nav>
+        <div style= {{"justify-self": "flex-start"}}>
         <NavLink to='/'>
-          <img src={require('../../images/logo.png')} alt='logo' />
+          Universidad de Extremadura
         </NavLink>
+        </div>
+
         <Bars />
         <NavMenu>
           <NavBtn>
@@ -27,8 +30,6 @@ const Navbar = () => {
           <NavBtn>
             <NavBtnLinkYellow to='/Aulas'>Aulas</NavBtnLinkYellow>
           </NavBtn>
-          {/* Second Nav */}
-          {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
         </NavMenu>
         <NavBtn>
           <NavBtnLinkGreen to='/Usuario'>Francisco FLores Solano</NavBtnLinkGreen>
--- src/pages/Horario.js
+++ src/pages/Horario.js
@@ -1,16 +1,42 @@
 import React from 'react';
+import { LateralMenu, Timetable, MenuHeader, MenuBody, FullWidthMenu, SpaceBetweenMenu, Button} from './PagesElements';
 
 const Horario = () => {
   return (
     <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '80vh'
-      }}
-    >
-      <h1>Horario</h1>
+    style={{
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'flex-end'
+    }}
+  >
+    <FullWidthMenu>
+      ASdasdasd
+    </FullWidthMenu>
+      <SpaceBetweenMenu>
+        <LateralMenu>
+          <MenuHeader>
+            <h2>Asignaturas</h2>
+          </MenuHeader>
+          <MenuBody>
+          </MenuBody>
+
+        </LateralMenu>
+        <Timetable>
+        <h1>Horario</h1>
+        </Timetable>
+        <LateralMenu>
+          <MenuHeader>
+            <h2>Asignaturas</h2>
+          </MenuHeader>
+          <MenuBody>
+          <Button>
+            Hola
+          </Button>
+          </MenuBody>
+
+        </LateralMenu>     
+      </SpaceBetweenMenu>
     </div>
   );
 };
