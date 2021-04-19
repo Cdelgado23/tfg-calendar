da336a8b823bf290bf654a10389c7e7247c41567
Carlos Delgado <cdelgadoguiberteau@gmail.com>
2021-04-19 21:15:10 +0200

    feat: use env file to store firestore configuration values (are not secret)

--- src/repository/Repository.js
+++ src/repository/Repository.js
@@ -14,12 +14,12 @@ export default class Repository{
         if (this.db == null){
         console.log("init repo");
         var firebaseConfig = {
-            apiKey: "AIzaSyDuOEZm-US7qOfa8-hb-eqsAfSFBw7NXMc",
-            authDomain: "tfg-calendar-310123.firebaseapp.com",
-            projectId: "tfg-calendar-310123",
-            storageBucket: "tfg-calendar-310123.appspot.com",
-            messagingSenderId: "409049602115",
-            appId: "1:409049602115:web:445470db2a0c15c8f61cf6"
+            apiKey:             process.env.REACT_APP_FIREBASE_API_KEY,
+            authDomain:         process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+            projectId:          process.env.REACT_APP_FIREBASE_PROJECT_ID,
+            storageBucket:      process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+            messagingSenderId:  process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+            appId:              process.env.REACT_APP_FIREBASE_APP_ID
           };
 
         firebase.initializeApp(firebaseConfig);
